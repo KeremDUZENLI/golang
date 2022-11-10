8033ed2d0819637e5ce1f8daba735671a0ae4c6a
KeremDUZENLI <krmdznl97@gmail.com>
2022-11-10 19:57:40 -0300

    2022.11.10

--- Golang Projects (TutorialEdge)/8-Calculator/main.go
+++ Golang Projects (TutorialEdge)/8-Calculator/main.go
@@ -1,35 +1,43 @@
 package main
 
 import (
-	"flag"
+	"strconv"
 	"syscall/js"
 )
 
-var (
-	listen = flag.String("listen", ":8080", "listen address")
-	dir    = flag.String("dir", ".", "directory to serve")
-)
-
 func add(i []js.Value) {
-	js.Global().Set("output", js.ValueOf(i[0].Int()+i[1].Int()))
-	println(js.ValueOf(i[0].Int() + i[1].Int()).String())
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	js.Global().Set("output", int1+int2)
+	println(int1 + int2)
 }
 
 func subtract(i []js.Value) {
-	js.Global().Set("output", js.ValueOf(i[0].Int()-i[1].Int()))
-	println(js.ValueOf(i[0].Int() - i[1].Int()).String())
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1-int2)
+	println(int1 - int2)
 }
 
-func registerCallbacks() {
-	js.Global().Set("add", js.NewCallback(add))
-	js.Global().Set("subtract", js.NewCallback(subtract))
-}
+// func registerCallbacks() {
+// 	js.Global().Set("add", js.NewCallback(add))
+// 	js.Global().Set("subtract", js.NewCallback(subtract))
+// }
 
 func main() {
 	c := make(chan struct{}, 0)
 
 	println("WASM Go Initialized")
-	// register functions
-	registerCallbacks()
+
+	// registerCallbacks()
+
 	<-c
 }
